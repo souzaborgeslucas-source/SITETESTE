5a2c7541ddc5104eeeca628706772e6c21fe2d6c
souzaborgeslucas-source <souza.borges.lucas@escola.pr.gov.br>
2026-04-27 14:41:56 +0000

    salvei 27-04

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("meubotao");
+
+    botao.addEventListener("click", function() {
+        alert("O botão foi clicado!");
+    });
+});
